dc01e32754b678f650ad426692e872413180aa6e
muntaha <muntahamirza890@gmail.com>
2025-04-03 23:14:15 +0500

    Added Ejs

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,11 +1,20 @@
 function getSignup(req , res, next){
+res.render('auth/signup',{ title: "Signup" })
+}
+function signup(req , res){
 
 }
+function login(req , res){
+    
+}
 function getlogin(req , res, next){
+    res.render('auth/login',{ title: "login" })
 
 }
 
 module.exports={
     getSignup : getSignup,
-    getlogin:getlogin
+    getlogin:getlogin,
+    signup :signup,
+    login:login
 }
